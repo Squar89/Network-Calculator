accf615440cce1d5114d0f03dfc8f540c7b02e16
Jakub Wróblewski <j.wroblewski97@gmail.com>
2019-10-08 13:21:24 +0200

    Listener mock

--- server.cpp
+++ server.cpp
@@ -5,24 +5,110 @@
 #include <stdexcept>
 #include <iostream>
 #include <cstring>
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+#include <thread>
 
 #define ARG_COUNT 5
 #define MAX_PORT 65535
 #define MAX_INCOMING_Q 30
+#define BUFFER_SIZE 99999
+
+/*
+template <typename T>
+class BlockingQ {
+
+private:
+    std::mutex              d_mutex;
+    std::condition_variable d_condition;
+    std::deque<T>           d_queue;
+
+public:
+    void push(T const& value) {
+        {
+            std::unique_lock<std::mutex> lock(this->d_mutex);
+            d_queue.push_front(value);
+        }
+        this->d_condition.notify_one();
+    }
+    T pop() {
+        std::unique_lock<std::mutex> lock(this->d_mutex);
+        this->d_condition.wait(lock, [=]{ return !this->d_queue.empty(); });
+        T rc(std::move(this->d_queue.back()));
+        this->d_queue.pop_back();
+        return rc;
+    }
+};
+*/
+
+void listenerFunction(struct sockaddr_in serverAddress) {
+    int serverSock, clientSock;
+    unsigned long addrLen;
+    struct sockaddr_in clientAddress;
+
+    /*DEBUG*/
+    char hello[BUFFER_SIZE];
+    memset(hello, 0, sizeof(hello));
+    strcpy(hello, "Hello from server");
+    char buffer[BUFFER_SIZE];
+    /*DEBUG*/
+
+    //open socket
+    if ((serverSock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Error occurred while opening a server socket\n";
+        return;
+    }
+
+    //bind socket
+    if (bind(serverSock, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error occurred while binding socket\n";
+        return;
+    }
+
+    //start listening for incoming connections on created socket
+    if (listen(serverSock, MAX_INCOMING_Q) == -1) {
+        std::cerr << "Error occurred while starting to listen on a socket\n";
+        return;
+    }
+
+    //start endless loop waiting for connections and handling them
+    while(true) {
+        //accept incoming connection and save it to clientSock
+        addrLen = sizeof(serverAddress);
+        if ((clientSock = accept(serverSock, (struct sockaddr*) &clientAddress, (socklen_t*) &addrLen)) < 0) {
+            std::cerr << "Error occurred while accepting incoming connection\n";
+            return;
+        }
+
+        if (recv(clientSock, buffer, 1024, 0) <= 0) {
+            std::cerr << "Error occurred while receiving data\n";
+            return;
+        }
+        std::cout << "Message received:\n" << buffer;
+
+        if (send(clientSock, hello, strlen(hello), 0) <= 0) {
+            std::cerr << "Error occurred while sending data\n";
+            return;
+        }
+        std::cout << "Message sent\n";
+    }
+
+    //same as thread joining, probably never going to happen but here for good measures
+    shutdown(clientSock, 0);
+    shutdown(serverSock, 0);
+
+    return;
+}
+
+void workerFunction(/*BlockingQ*/int placeholder) {
+    return;
+}
 
 int main(int argc, char *argv[]) {
     unsigned portNumber;
     unsigned long workersNumber;
-    struct sockaddr_in serverAddress, clientAddress;
-    int serverSock, clientSock;
-    unsigned long addrLen;
-
-    /*DEBUG*/
-    char hello[1024];
-    memset(hello, 0, sizeof(hello));
-    strcpy(hello, "Hello from server");
-    char buffer[1024] = {0};
-    /*DEBUG*/
+    struct sockaddr_in serverAddress;
 
     //check for required command line arguments
     if (argc == ARG_COUNT
@@ -71,47 +157,21 @@ int main(int argc, char *argv[]) {
     serverAddress.sin_port = htons(portNumber);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    //open socket
-    if ((serverSock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        std::cerr << "Error occurred while opening a server socket\n";
-        return -1;
+    std::thread workerThread[workersNumber];
+    //start worker threads
+    for (unsigned long i = 0; i < workersNumber; i++) {
+        workerThread[i] = std::thread(workerFunction, i);
     }
 
-    //bind socket
-    if (bind(serverSock, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1) {
-        std::cerr << "Error occurred while binding socket\n";
-        return -1;
-    }
+    //start listener thread
+    std::thread listenerThread(listenerFunction, serverAddress);
 
-    //start listening for incoming connections on created socket
-    if (listen(serverSock, MAX_INCOMING_Q) == -1) {
-        std::cerr << "Error occurred while starting to listen on a socket\n";
-        return -1;
+    //this will probably never happen as both listener and worker operates in endless loop
+    //but I placed it here for good measures - join created threads
+    listenerThread.join();
+    for (unsigned long i = 0; i < workersNumber; i++) {
+        workerThread[i].join();
     }
-
-    //accept incoming connection and save it to clientSock
-    addrLen = sizeof(serverAddress);
-    if ((clientSock = accept(serverSock, (struct sockaddr*) &clientAddress, (socklen_t*) &addrLen)) < 0) {
-        std::cerr << "Error occurred while accepting incoming connection\n";
-        return -1;
-    }
-
-    /*DEBUG*/
-    if (recv(clientSock, buffer, 1024, 0) <= 0) {
-        std::cerr << "Error occurred while receiving data\n";
-        return -1;
-    }
-    std::cout << "Message received:\n" << buffer;
-
-    if (send(clientSock, hello, strlen(hello), 0) <= 0) {
-        std::cerr << "Error occurred while sending data\n";
-        return -1;
-    }
-    std::cout << "Message sent\n";
-    /*DEBUG*/
-
-    shutdown(clientSock, 0);
-    shutdown(serverSock, 0);
     
     return 0;
 }
